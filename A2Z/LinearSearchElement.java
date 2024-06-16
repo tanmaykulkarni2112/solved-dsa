c8a4b45ece708778b2baaaef1a5ac8d45402cb19
tanmaykulkarni2112 <149245022+tanmaykulkarni2112@users.noreply.github.com>
2024-06-16 17:02:51 +0530

    Create LinearSearchElement.java

---
+++ A2Z/LinearSearchElement.java
@@ -0,0 +1,12 @@
+//g4g code
+class Solution{
+    static int searchInSorted(int arr[], int N, int K)
+    {
+        for(int i = 0; i < N; i++){
+            if(arr[i] == K){
+                return 1;
+            }
+        }
+        return -1;
+    }
+}
