d89ec60b379b6a0de2698c1035ad8ad0b79e4119
tanmaykulkarni2112 <149245022+tanmaykulkarni2112@users.noreply.github.com>
2024-06-18 16:36:29 +0530

    Update MajorityElement.java- Moore

--- A2Z/MajorityElement.java
+++ A2Z/MajorityElement.java
@@ -1,5 +1,34 @@
-//retry with  Boyer-Moore Voting Algorithm later
+//moore voting algorithm
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int element = 0;
+        int freq = 0;
+        for(int i = 0; i < nums.length; i++) { 
+            if(count == 0) {
+                element = nums[i];
+                count = 1; 
+            } 
+            else if(element == nums[i]) {
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(element == nums[i]) {
+                freq++;
+            }
+        }
+        if(freq > nums.length/2) {
+            return element;
+        }
+        return -1;
+    }
+}
 
+//non optimal
 class Solution {
     public int majorityElement(int[] nums) {
         HashMap <Integer, Integer> map = new HashMap<>();
