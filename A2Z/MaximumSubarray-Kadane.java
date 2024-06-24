24d57dc6308e268c67c6d4d27314ef50cd396525
tanmaykulkarni2112 <149245022+tanmaykulkarni2112@users.noreply.github.com>
2024-06-24 19:32:44 +0530

    Create MaximumSubarray-Kadane.java

---
+++ A2Z/MaximumSubarray-Kadane.java
@@ -0,0 +1,18 @@
+//kadane algorithm
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        int max = nums[0];
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            if(sum > max){
+                max = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return max;
+    }   
+}
