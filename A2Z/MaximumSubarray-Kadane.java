813857cdbc968be320034d3873ae2fd161a28609
tanmaykulkarni2112 <149245022+tanmaykulkarni2112@users.noreply.github.com>
2024-06-24 19:57:05 +0530

    Update MaximumSubarray-Kadane.java

--- A2Z/MaximumSubarray-Kadane.java
+++ A2Z/MaximumSubarray-Kadane.java
@@ -1,3 +1,23 @@
+//g4g problem to be understood 
+//https://www.geeksforgeeks.org/problems/max-sum-in-sub-arrays0824/0?category
+class Solution {
+    
+    public static long pairWithMaxSum(long arr[], long N)
+    {
+        long sum = Long.MIN_VALUE;
+        long max = Long.MIN_VALUE;
+        for (int i = 0; i < N-1; i++){
+            sum = arr[i] + arr[i+1];
+            if(sum > max){
+                max = sum;
+            }
+            
+        }
+        return max;
+    }
+}
+
+
 //kadane algorithm
 class Solution {
     public int maxSubArray(int[] nums) {
